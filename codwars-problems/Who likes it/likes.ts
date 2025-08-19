b072d639d462d339733412c4ff88d94e24d8b916
sashok_meer <aleksandrme3@gmail.com>
2025-08-19 13:43:10 +0400

    rewrite likes with object of formats

--- codwars-problems/Who likes it/likes.ts
+++ codwars-problems/Who likes it/likes.ts
@@ -1,14 +1,9 @@
 const likes = (a : string[]) : string => {
-  switch (a.length) {
-    case 0:
-      return "no one likes this";
-    case 1:
-      return `${a[0]} likes this`;
-    case 2:
-      return `${a[0]} and ${a[1]} like this`;
-    case 3:
-      return `${a[0]}, ${a[1]} and ${a[2]} like this`;
-    default:
-      return `${a[0]}, ${a[1]} and ${a.length - 2} others like this`
-  }
+  const formats: { [key: number]: (arr: string[]) => string } = {
+    0: () => "no one likes this",
+    1: (arr) => `${arr[0]} likes this`,
+    2: (arr) => `${arr[0]} and ${arr[1]} like this`,
+    3: (arr) => `${arr[0]}, ${arr[1]} and ${arr[2]} like this`
+  };
+  return (formats[a.length] || ((arr) => `${a[0]}, ${a[1]} and ${a.length - 2} others like this` ))(a);
 }
