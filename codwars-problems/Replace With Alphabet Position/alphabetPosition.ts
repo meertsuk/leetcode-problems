cc1f518f6472cb9995505dc820afe1531bdf325a
sashok_meer <aleksandrme3@gmail.com>
2025-08-19 13:19:35 +0400

    rewrite alphabetPosition with reduce

--- codwars-problems/Replace With Alphabet Position/alphabetPosition.ts
+++ codwars-problems/Replace With Alphabet Position/alphabetPosition.ts
@@ -1,9 +1,12 @@
 function alphabetPosition(text:string):string {
-  let result: number [] = [];
-  for (let char of text.toLowerCase()) {
-    if (/[a-z]/.test(char)) {
-      result.push(char.charCodeAt(0) - 96);
-    }
-  }
-  return result.join(' ');
+  return text
+    .toLowerCase()
+    .split('')
+    .reduce((acc, char) => {
+      if (/[a-z]/.test(char)) {
+        acc.push(char.charCodeAt(0) - 96);
+      }
+      return acc;
+    }, [])
+    .join(' ')
 }
